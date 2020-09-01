5c2fddd9114787f7c114ed7a2595341ad454f578
niknikich <82ann8@mail.ru>
2020-09-01 17:13:00 +0300

    edit dto 3

--- src/modules/gates/dto/request/state_update.dto.ts
+++ src/modules/gates/dto/request/state_update.dto.ts
@@ -7,6 +7,6 @@ export class SateUpdateDto extends ConstructableDto {
   @IsNotEmpty()
   @IsEnum(GATE_STATUSES)
   @IsNumber()
-  @ApiPropertyOptional({enum: GATE_STATUSES, description: 'State gate'})
+  @ApiPropertyOptional({type: Number, description: 'State gate'})
   state: number;
 }
--- src/modules/gates/dto/request/update_gate.dto.ts
+++ src/modules/gates/dto/request/update_gate.dto.ts
@@ -14,7 +14,7 @@ export class UpdateGateDto extends ConstructableDto {
   @IsEnum(GATE_STATUSES)
   @IsOptional()
   @IsNumber()
-  @ApiPropertyOptional({enum: GATE_STATUSES, description: 'State gate'})
+  @ApiPropertyOptional({type:Number, description: 'State gate'})
   state?: number;
 
   @IsOptional()
